5f591d96cc870c5eda8ba052ecd95e5a7fcd2f8d
houssniismail <houssniismail6@gmail.com>
2023-04-14 18:44:58 +0200

    melolibrary

--- mazicaZoom/resources/views/livewire/piece-show.blade.php
+++
@@ -1,6 +0,0 @@
-<div>
-    <input type="text" wire:model="searchArtist" placeholder="Search Artist">
-    <input type="text" wire:model="searchTitre" placeholder="Search Titre">
-    <button wire:click="search">Search</button>
-</div>
-
